84a90be1cd43176d227748e0a9359269d44ab9b0
mansourlahham14 <mansourlahham14@gmail.com>
2026-02-07 03:07:29 +0100

    MODIFICHE

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Layout from './components/Layout'
-import Home from './components/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
+import Layout from './Components/Layout'
+import Home from './Components/Home'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
 
 const App = () => {
   return (
